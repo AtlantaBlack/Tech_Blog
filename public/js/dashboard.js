d949021bd1d235bf5705a8be2fbd9b27eeaca1f7
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-05-31 22:43:21 +1000

    added visibility toggle to blog post form

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,33 +1,47 @@
+const toggleFormButton = document.getElementById("toggle-form-vis");
+
+// show the form on click
+const showBlogPostForm = () => {
+	let form = document.getElementById("new-post-form");
+
+	console.log("CLICK");
+	console.log(form);
+
+	form.classList.toggle("hide");
+};
+
 // create new post
 const addNewBlogPost = async (event) => {
-  event.preventDefault();
+	event.preventDefault();
 
-  console.log('DASHBOARD NEW POST CLICK');
+	console.log("DASHBOARD NEW POST CLICK");
 
-  const postTitle = document.getElementById('post-title').value.trim();
-  const postContent = document.getElementById('post-content').value.trim();
+	const postTitle = document.getElementById("post-title").value.trim();
+	const postContent = document.getElementById("post-content").value.trim();
 
-  if (postTitle && postContent) {
-    const response = await fetch('/api/posts', {
-      method: 'POST',
-      body: JSON.stringify({ postTitle, postContent }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+	if (postTitle && postContent) {
+		const response = await fetch("/api/posts", {
+			method: "POST",
+			body: JSON.stringify({ postTitle, postContent }),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
 
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert('Blog post failed.');
-    }
-  }
+		if (response.ok) {
+			document.location.replace("/dashboard");
+		} else {
+			alert("Blog post failed.");
+		}
+	}
 };
 
 // update old post
 
 // delete old post
 
+toggleFormButton.addEventListener("click", showBlogPostForm);
+
 document
-  .getElementById('new-post-form')
-  .addEventListener('submit', addNewBlogPost);
+	.getElementById("new-post-form")
+	.addEventListener("submit", addNewBlogPost);
