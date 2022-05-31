75187434196c17215a1bc2927e617b12994438db
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-05-31 23:26:27 +1000

    added update and delete buttons in dashboard hbs (no js)

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,59 +1,67 @@
-const router = require('express').Router();
-const { User, Post, Comment } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-// get all posts
-router.get('/', async (req, res) => {
-  try {
-    const postData = await Post.findAll({
-      include: [
-        { model: User },
-        { model: Comment }
-      ]
-    });
+// get all blog posts
+router.get("/", async (req, res) => {
+	try {
+		const postData = await Post.findAll({
+			include: [{ model: User }, { model: Comment }]
+		});
 
-    res.status(200).json(postData);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+		res.status(200).json(postData);
+	} catch (error) {
+		res.status(500).json(error);
+	}
 });
 
-router.post('/', withAuth, async (req, res) => {
-  try {
+// post a new blog post
+router.post("/", withAuth, async (req, res) => {
+	try {
+		// console.log(`\n---POST ROUTE: POST NEW BLOG`);
+		// console.log(req.body);
 
-    // console.log(`\n---POST ROUTE: POST NEW BLOG`);
-    // console.log(req.body);
+		// const postAuthor = await User.findOne({
+		//   where: {
+		//     id: req.session.user_id
+		//   }
+		// });
 
-    // const postAuthor = await User.findOne({
-    //   where: {
-    //     id: req.session.user_id
-    //   }
-    // });
+		// console.log(`\n---POST ROUTE: POST AUTHOR`);
+		// console.log(postAuthor);
+		// console.log(postAuthor.username);
 
-    // console.log(`\n---POST ROUTE: POST AUTHOR`);
-    // console.log(postAuthor);
-    // console.log(postAuthor.username);
+		// const { username } = postAuthor;
 
-    // const { username } = postAuthor;
+		// const { username } = await User.findOne({
+		//   where: {
+		//     id: req.session.user_id
+		//   }
+		// });
 
-    // const { username } = await User.findOne({
-    //   where: {
-    //     id: req.session.user_id
-    //   }
-    // });
+		const newPost = await Post.create({
+			...req.body,
+			user_id: req.session.user_id
+		});
 
-    const newPost = await Post.create({
-      ...req.body,
-      user_id: req.session.user_id
-    });
+		// console.log(`\n---POST ROUTE: NEW POST DATA`);
+		// console.log(newPost);
 
-    // console.log(`\n---POST ROUTE: NEW POST DATA`);
-    // console.log(newPost);
+		res.status(200).json(newPost);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+});
 
-    res.status(200).json(newPost);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+//update a blog post
+router.put("/:id", withAuth, async (req, res) => {
+	try {
+		console.log(req.body);
+	} catch (error) {
+		console.log(`\n----POST ROUTE: POST ID ERROR`);
+		console.log(error);
+		res.status(500).json(error);
+	}
 });
 
 module.exports = router;
