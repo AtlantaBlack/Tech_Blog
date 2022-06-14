251b7d2e771c7f8ea5e8bf4740506ccb25f242d4
Atlanta Black <97859552+AtlantaBlack@users.noreply.github.com>
2022-06-14 12:31:23 +1000

    added comments to comment routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -5,27 +5,28 @@ const withAuth = require("../../utils/auth");
 // post a new comment
 router.post("/", withAuth, async (req, res) => {
 	try {
+		// console.log(`\n---COMMENT ROUTE: POST NEW COMMENT`);
+		// console.log(req.body);
+		// console.log("\nHere is req.session:", req.session.user_id, "\n");
 
-		console.log(`\n---COMMENT ROUTE: POST NEW COMMENT`);
-		console.log(req.body);
-		console.log("\nHere is req.session:", req.session.user_id, "\n");
-
+		// get contents out of req.body
 		const { commentContent, postId } = req.body;
-
+		// get user id
 		const userId = req.session.user_id;
 
+		// make new comment
 		const newComment = await Comment.create({
 			comment_content: commentContent,
 			post_id: postId,
 			user_id: userId
 		});
 
-		console.log(`\n---COMMENT ROUTE: NEW COMMENT`);
-		console.log(newComment);
+		// console.log(`\n---COMMENT ROUTE: NEW COMMENT`);
+		// console.log(newComment);
 
 		res.status(200).json(newComment);
 	} catch (error) {
-    console.log(`\n----COMMENT ROUTE: POST NEW COMMENT ERR`);
+		console.log(`\n----COMMENT ROUTE: POST NEW COMMENT ERR`);
 		console.log(error);
 		res.status(500).json(error);
 	}
